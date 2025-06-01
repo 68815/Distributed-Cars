a018d211114dba2bf893dc5b3ca85f245d3601b2
0109L_ <Axty0903@outlook.com>
2025-06-01 20:34:00 +0800

    修改终点选举逻辑，带有随机性

--- src/main/java/ncepusa/distributedcars/navigator/data_structures/GridMap.java
+++ src/main/java/ncepusa/distributedcars/navigator/data_structures/GridMap.java
@@ -140,67 +140,92 @@ public class GridMap{
      * 选择具有较多未探索点的区域作为终点。
      * 终点的选择基于每个节点周围未探索邻居的数量。
      */
-    public void electEndpoint(int carNumbers, int carid) {
-        GridNode tmpNode;
+    public void electEndpoint(int carNumber, int carid) {
         int maxUnexploredCount = -1;
-        Random random = new Random();
-        int heightCount = carNumbers / 2;
-        int heightLength = 0;
-        if((carNumbers & 1) == 1) heightLength = (int) ((double)height / (0.5 + (double)heightCount));
-        else heightLength = height / heightCount;
-        int startx = (((carid & 1) == 0) ? width / 2 : 0);
-        int endx = (startx == 0 ? width / 2 : width);
-        int starty = ((carid - 1) / 2) * heightLength;
-        int endy = starty + heightLength;
-        if(carNumbers == 1) {
-            startx = 0;
-            endx = width;
-            starty = 0;
-            endy = height;
-        }
-        else if((carNumbers & 1) == 1 && carid == carNumbers) {
-            startx = 0;
-            endx = width;
-            starty = heightLength * heightCount - 1;
-            endy = height;
-        }
-        st : for(int j = (PrimesUtil.isPrime(carid) ? starty : (endy - 1));
-                 j >= starty && j < endy;
-                 j += (PrimesUtil.isPrime(carid) ? 1 : -1)) {
-            for (int i = (start.getX() < (endx + startx) / 2 ? (endx - 1) : startx);
-                 i < endx && i >= startx;
-                 i += (start.getX() < (endx + startx) / 2 ? -1 : 1)) {
-                if (i == start.getX() && j == start.getY()) continue;
-                tmpNode = grid.get(j).get(i);
-                if (!tmpNode.isObstacle() && tmpNode.isArrived()) {
-                    int exploredCount = countUnexploredNeighbors(tmpNode);
-                    if (exploredCount > maxUnexploredCount) {
-                        maxUnexploredCount = exploredCount;
-                        this.end = new Point(j, i);
-                        if (maxUnexploredCount == 9) break st;
+        Point candidatePoint = null;
+        Random rand = new Random();
+        GridNode tmpNode;
+        //第一层随机：倾向于探索远处的点还是近处的点？
+        boolean isFar = rand.nextBoolean();
+        //第二层随机：优先选择左右还是上下?
+        boolean isLeftRight = rand.nextBoolean();
+        //第三层随机：随机一个起始点,优先外围点
+        int i = isLeftRight ?
+                (isFar ?
+                        ((int)start.getX() < width / 2 ?
+                                width - 1 : 0) :
+                        ((int)start.getX() < width / 2 ?
+                                0 : width - 1))
+                : rand.nextInt(width);
+        int j = isLeftRight ?
+                rand.nextInt(height)
+                : (isFar?
+                ((int)start.getY() < height / 2 ?
+                        height - 1 : 0) :
+                ((int)start.getY() < height / 2 ?
+                        0 : height - 1));
+        int count = width * 2 + height * 2 - 4;
+        int times = 0;
+        //从外围开始，逐渐向中心靠近
+        st:while(count > 0)
+        {
+            int ii = i;
+            int jj = j;
+            //顺时针
+            int directions = 0;//方向
+            if(j == times && i != width - 1 - times) directions = 1;//向右
+            else if(i == width - 1 - times && j != height - 1 - times) directions = -2;//向下
+            else if(j == height - 1 - times && i != times) directions = -1;//向左
+            else if(i == times && j != times) directions = 2;//向上
+            for(int k = 0; k < count; k++)
+            {
+                if((directions & 1) == 0) {
+                    if(ii + directions < times){
+                        directions = 2;
+                        k--;
+                        continue;
                     }
+                    else if(ii + directions > width - 1 - times) {
+                        directions = -2;
+                        k--;
+                        continue;
+                    }
+                    else ii += directions;
+                }
+                else {
+                    if(jj + directions < times){
+                        directions = 1;
+                        k--;
+                        continue;
+                    }
+                    else if(jj + directions > height - 1 - times) {
+                        directions = -1;
+                        k--;
+                        continue;
+                    }
+                    else jj += directions / 2;
+                }
+                if(ii == start.getX() && jj == start.getY()) continue;
+                tmpNode = grid.get(jj).get(ii);
+                int unexploredCount = countUnexploredNeighbors(tmpNode);
+                if (unexploredCount > maxUnexploredCount ||
+                        (unexploredCount == maxUnexploredCount && maxUnexploredCount == 9 && rand.nextDouble() < 0.9) ||
+                        (unexploredCount < maxUnexploredCount && rand.nextDouble() < 0.1)) {
+                    maxUnexploredCount = unexploredCount;
+                    candidatePoint = new Point(jj, ii);
+                    if (maxUnexploredCount == 9 && rand.nextDouble() >= 0.9) break st;
                 }
             }
+            if(i == times) i++;
+            else if(i == width - 1 - times) i--;
+            if(j == times) j++;
+            else if(j == height - 1 - times) j--;
+            count -= 8;
+            times++;
         }
-        logger.info("maxUnexploredCount: {}", maxUnexploredCount);
-        //当前carid的小车已探索完 分配给它的区域
-        if (null == this.end) {
-            maxUnexploredCount = -1;
-            st : for(int j = 0; j < height; j++){
-                for(int i = 0; i < width; i++){
-                    if (i == start.getX() && j == start.getY()) continue;
-                    tmpNode = grid.get(j).get(i);
-                    if(!tmpNode.isObstacle() && tmpNode.isArrived()) {
-                        int exploredCount = countUnexploredNeighbors(tmpNode);
-                        if (exploredCount > maxUnexploredCount) {
-                            maxUnexploredCount = exploredCount;
-                            this.end = new Point(j, i);
-                            if (maxUnexploredCount == 9 && random.nextDouble() <= 0.9) break st;
-                        }
-                    }
-                }
-            }
-            logger.info("maxUnexploredCount: {}", maxUnexploredCount);
+        logger.info("maxUnexploredCount:{}", maxUnexploredCount);
+        if (candidatePoint != null) {
+            this.end = candidatePoint;
         }
     }
     /**
